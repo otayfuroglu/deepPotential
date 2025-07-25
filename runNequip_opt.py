521eb1f54cbf9492351631f5535976e7fa46727a
nesmamutlu <nesmamutlu@gmail.com>
2025-07-25 11:38:21 +0300

    optimization added

--- runNequip_opt.py
+++ runNequip_opt.py
@@ -8,9 +8,23 @@ import argparse
 from ase.optimize import BFGS
 
 
+
+def getBoolStr(string):
+    string = string.lower()
+    if "true" in string or "yes" in string:
+        return True
+    elif "false" in string or "no" in string:
+        return False
+    else:
+        print("%s is bad input!!! Must be Yes/No or True/False" %string)
+        sys.exit(1)
+
+
+
 parser = argparse.ArgumentParser(description="Give something ...")
 parser.add_argument("-extxyz_path", type=str, required=True, help="")
 parser.add_argument("-model_path", type=str, required=True, help="")
+parser.add_argument("-opt", type=str, required=True, help="")
 args = parser.parse_args()
 
 device = "cuda"
@@ -18,6 +32,8 @@ device = "cuda"
 
 extxyz_path = args.extxyz_path
 model_path = args.model_path
+opt = getBoolStr(args.opt)
+
 #  file_base = extxyz_path.split("/")[-1].split(".")[0]
 file_base = model_path.split("/")[-1].split(".")[0]
 
@@ -28,21 +44,22 @@ calc = NequIPCalculator.from_deployed_model(
 
 atoms_list = read(extxyz_path, index=":")
 
-fl_enegies = open(f"{file_base}_qm_model_energeis.csv", "w")
-fl_enegies.write(f"index,e_QM,e_Model,e_diff\n")
+fl_enegies = open(f"{file_base}_qm_model_opt{args.opt}_energeis.csv", "w")
+fl_enegies.write(f"Name,NNP_Energy\n")
 
 #  while n_sample <= 250:
-for i in tqdm.trange(0, len(atoms_list), 1):
-    #  for i in range(0, len(atoms_list), 1):
+for i in tqdm.trange(0,len(atoms_list), 1):
+#for i in range(0, len(atoms_list), 1):
+    atoms = atoms_list[i]
     try:
         label = atoms.info["label"]
     except:
         label = f"frame_{i}"
 
-    atoms = atoms_list[i]
     atoms.calc = calc
-    dyn = BFGS(atoms)
-    dyn.run(fmax=0.001)
+    if opt:
+        dyn = BFGS(atoms)
+        dyn.run(fmax=0.001)
     model_energy = atoms.get_potential_energy()
     #energy_diff = model_energy - qm_enery
 
@@ -50,7 +67,6 @@ for i in tqdm.trange(0, len(atoms_list), 1):
     #  diff_forces = abs(qm_forces - model_forces)
 
     fl_enegies.write(f"{label},{model_energy}\n")
-    fl_enegies.flush()
     fl_enegies.flush()
 
 
