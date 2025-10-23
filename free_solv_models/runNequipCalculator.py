935959cb91c99a3f673e2c192de320dcce5a5f6f
esma <esma@MacBook-Air.local>
2025-10-23 13:30:55 +0300

    Add Nequip Calculator

--- free_solv_models/runNequipCalculator.py
+++ free_solv_models/runNequipCalculator.py
@@ -41,7 +41,7 @@ calc = NequIPCalculator.from_deployed_model(
 
 atoms_list = read(extxyz_path, index=":")
 
-fl_enegies = open(f"{file_base}_qm_model_opt{args.opt}_energeis.csv", "w")
+fl_enegies = open(f"{file_base}_qm_model_energeis.csv", "w")
 fl_enegies.write(f"Name,NNP_Energy\n")
 
 for i in tqdm.trange(0,len(atoms_list), 1):
@@ -52,9 +52,6 @@ for i in tqdm.trange(0,len(atoms_list), 1):
         label = f"frame_{i}"
 
     atoms.calc = calc
-    if opt:
-        dyn = BFGS(atoms)
-        dyn.run(fmax=0.001)
     model_energy = atoms.get_potential_energy()
 
 
